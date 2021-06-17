8341ccd990b295e601249524723582319c2b50cb
Faiz Authar <faizauthar@gmail.com>
2021-06-17 20:49:44 +0700

    MovieCatalogue: HomeScreen: barebone code of FlatList * Width and Height will be dynamically changes based on device Dimension * using dummy data * due to background app will be immediately grayed, use view and add style background color "white" to fix it.

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,17 +1,77 @@
 import React from "react";
-import { Text, StyleSheet, View} from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Dimensions, Image} from "react-native";
+import Movies from "../dummy/MoviesData";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+// debugging
+//console.log(windowWidth);
+//console.log(windowHeight);
 
 const HomeScreen = () => {
   return (
-    <View style={styles.view}>
-        <Text>Home Screen</Text>
+    <View style={styles.viewStyles}>
+      <FlatList
+        data={Movies}
+        keyExtractor={Movie => Movie.name}
+
+        renderItem={({ item }) => {
+          return(
+            <TouchableOpacity 
+              style={styles.Rectangle}
+              onPress={()=>{}}>
+              <Image
+                style={styles.Image}
+                source={require('../../assets/movie.jpg')}
+              />
+              <View style={styles.viewStylesChild1}>
+                <Text>
+                  <Text style={styles.textHighlight}>{item.name}</Text>({item.release})
+                </Text>
+                <Text numberOfLines={4}>
+                  {item.desc}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )
+        }}
+      />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  view: {
-    margin: 10
+  viewStyles: {
+    backgroundColor: "white",
+    alignItems: "center"
+  },
+  viewStylesChild1: {
+    flexDirection: "column",
+    flex: 2,
+    marginStart: 10
+  },
+  Rectangle:{
+    borderRadius: 10,
+    margin: 5,
+    padding: 20,
+    width: windowWidth / 1.1,
+    height: windowHeight / 5.5,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 1, height: 1},
+    shadowRadius: 10,
+    elevation: 3,
+    backgroundColor: "white",
+    flexDirection: "row"
+  },
+  Image: {
+    width: windowWidth/6,
+    height: windowWidth/4,
+    justifyContent: 'center',
+  },
+  textHighlight: {
+    fontWeight: "700",
   }
 });
 
